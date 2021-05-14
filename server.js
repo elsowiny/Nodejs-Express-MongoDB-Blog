b7e4d47b6c4d0bf605e43a5a4cc479d682e49861
Sherief Elsowiny <elsowiny@yahoo.com>
2021-05-14 17:13:01 -0400

    added routes and working on integrating mongoose and adding an article soon

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const articleRouter = require('./routes/articles')
 const app = express()
 
 app.set('view engine', 'ejs')
 
+app.use('/articles', articleRouter)
+//here is the base for our blog -
+//what we have is that our const articles, will contain all of the articles to which we
+//will iterate through
 app.get('/',(req, res) => {
-    res.render('index');
+
+    const articles = [{
+        title: 'Test Article',
+        createdAt: new Date(),
+        description: 'Test description'
+    },
+    {
+        title: 'Test Article3',
+        createdAt: new Date(),
+        description: 'Test description'
+    }
+
+]
+    res.render('articles/index', { articles: articles});
 })
 app.listen(5000)
