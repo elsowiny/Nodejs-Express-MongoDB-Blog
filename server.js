a62c4079a195f18fd94f593bc964c7ff1b9157e2
Sherief Elsowiny <elsowiny@yahoo.com>
2021-05-15 12:34:21 -0400

    added blog slugs and now finishing up

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const app = express()
 
 mongoose.connect('mongodb://localhost/blog', {useNewUrlParser: true, useUnifiedTopology: true})
 app.set('view engine', 'ejs')
+//this allows us to acess our form options
+app.use(express.urlencoded({extended: false}))
 
-app.use('/articles', articleRouter)
 //here is the base for our blog -
 //what we have is that our const articles, will contain all of the articles to which we
 //will iterate through
-app.get('/',(req, res) => {
+app.get('/', async (req, res) => {
 
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Test description'
-    },
-    {
-        title: 'Test Article3',
-        createdAt: new Date(),
-        description: 'Test description'
-    }
-
-]
+   const articles = await Article.find().sort({createdAt: 'desc'})
     res.render('articles/index', { articles: articles});
 })
+
+app.use('/articles', articleRouter)
+
 app.listen(5000)
