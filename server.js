70b34482770f6ffbd27971c718dcb58475df044d
Sherief Elsowiny <elsowiny@yahoo.com>
2021-05-15 12:54:05 -0400

    added delete functionality

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
-mongoose.connect('mongodb://localhost/blog', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost/blog', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 app.set('view engine', 'ejs')
 //this allows us to acess our form options
 app.use(express.urlencoded({extended: false}))
+
+app.use(methodOverride('_method'))
 
 //here is the base for our blog -
 //what we have is that our const articles, will contain all of the articles to which we
